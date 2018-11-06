8fd2118beda2628624f23ab0391cbc3e09172cd1
Jarrian McLean <jarrian.mclean@gmail.com>
2018-11-06 01:34:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def roll_call_dwarves(array)
 
 def summon_captain_planet(planteer_calls)
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-  planteer_calls.collect do |element|
+  planteer_calls.each do |element|
      element.upcase + "!"
 end
 end
