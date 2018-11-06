1ffa4ef97a2a61dd0fc81135b9b68a4a9b75dca8
Jarrian McLean <jarrian.mclean@gmail.com>
2018-11-06 02:04:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ array.any? do |call|
 end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese_types.include?(cheese)
 end
